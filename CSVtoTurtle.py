8fa95d98e1b44491233a20766a616e7dfc416cb3
gheinrich <Guilhem.Heinirch@inra.fr>
2017-12-12 16:39:27 +0100

    first commit

---
+++ CSVtoTurtle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+""" This module aims at converting a csv file into a RDF graph in turtle syntax.
+    More specificly it will read the columns : 
+    and parse them using the given associative array
+"""
+
+import argparse
+import json
+import csv
+
+class CSVtoTurtleConverter(object):
+    """ A class to convert a csv to a rdf turtle file"""
+    def __init__(self, prefix = None, base = None, assocRules = None):
+        self.assoc_rules = assocRules
+        self.prefix = prefix
+        self.base = base
+    def parse_csv(self, csvFile, turtleFile):
+        """This function read a csv file and parse its content as a turtle rdf file"""
+        with open(csvFile) as csvfile:
+            with open(turtleFile, 'w') as turtlefile:
+                reader = csv.DictReader(csvfile)
+                turtlefile.write(self.prefix)
+                turtlefile.write('\n')
+                turtlefile.write(self.base)
+                turtlefile.write('\n \n')
+                for row in reader:
+                    for rule in self.assoc_rules:
+                        line = rule['prefix'].format(**row) + row[rule['colname']] + rule['postfix'].format(**row) + "\n"
+                        turtlefile.write(line)
+                        print (line)
+                    turtlefile.write('\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = """ This module aims at converting a csv file into a RDF graph in turtle syntax.
+    More specificly it will read the columns : 
+    and parse them using the given associative array
+""")
+    parser.add_argument('configJSON', help="""Path to the configuration file to parse the csv, in json format.""")
+    parser.add_argument('-i', dest = 'input', help="""Path to the input CSV file""")
+    parser.add_argument('-o', dest = 'output', help="""Path to the output turtle file""")
+    args = parser.parse_args()
+    with open(args.configJSON, 'r') as jsonconfigfile:
+        config = json.load(jsonconfigfile)
+        converter = CSVtoTurtleConverter(' \n'.join(config['prefix']), config['base'], config['associativeRules'])
+        converter.parse_csv(args.input, args.output)
+        
+
+    
+# prefix = """
+# @prefix : <http://www.phenome-fppn.fr/vocabulary/m3p/2015/event#> .
+# @prefix owl: <http://www.w3.org/2002/07/owl#> .
+# @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+# @prefix xml: <http://www.w3.org/XML/1998/namespace> .
+# @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+# @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+# """
+
+# base = """
+# @base <http://www.phenome-fppn.fr/vocabulary/m3p/2015/event> . \n
+# """
+
+# associativeRules = [{'colname' : 'Category','prefix' : '_:event rdf:type :', 'postfix' : ' ;'},
+# {'colname' : 'uri', 'prefix' : '    :concern <', 'postfix' : '> ;'},
+# {'colname' : 'Date event', 'prefix' : '    :inDateTime "', 'postfix' : '" .'},
+# ]
+
+
+# myConv = CSVtoTurtleConverter(prefix, base, associativeRules)
+# myConv.parse_csv('Events ARCH2017-03-30.csv', 'Events ARCH2017-03-30.ttl')
